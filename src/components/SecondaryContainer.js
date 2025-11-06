9861bea4c2326778cc76276041bbae4664e369a0
NIKHIL KUMAR PRAJAPATI <np1626978@gmail.com>
2025-11-06 15:40:59 +0530

    margin resolved

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -12,7 +12,7 @@ const SecondaryContainer = () => {
   return (
    
       <div className="bg-black">
-        <div className="mt-0 md:-mt-52 pl-4 md:pt-12 relative z-20 ">
+        <div className="mt-0 md:-mt-auto pl-4 md:pt-12 relative z-20 ">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
           <MovieList title={"Trending"} movies={movies.trendingMovies} />
           <MovieList title={"Popular"} movies={movies.popularMovies} />
